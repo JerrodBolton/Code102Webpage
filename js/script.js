1bcb8b59867b5e04345859967379c861b2f4f39c
acanady <alfonso.canadyjr@gmail.com>
2021-06-30 15:47:14 -0500

    Test

---
+++ js/script.js
@@ -0,0 +1,37 @@
+console.log("Hello World");
+console.log("Alfonso");
+
+//declaring variable x, setting it to 20
+var x; //variable declaration
+x = 20;//varaible assignment
+
+console.log(x); //Prints out the value stored in x
+
+var myName = "Christopher";
+console.log(myName);
+
+var my_age = 52;
+
+var userAge = prompt("How old are you?");
+console.log(userAge);
+
+if (userAge >= 21){
+  var userName = prompt("What is your name?");
+  document.write("Welcome " + userName + "!");
+}
+else if(userAge >= 13){
+  var userName = prompt("What is your name?");
+  if(userName == "James"){
+    alert("Hello there James");
+  }
+}
+else{
+  alert("You need parental permission");
+}
+
+
+
+
+
+
+
