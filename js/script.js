97f3bcfe700b7deae08f2b0e2552f3058565c0dc
acanady <alfonso.canadyjr@gmail.com>
2021-07-01 13:15:35 -0500

    added function notes and edited js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+/*console.log("Hello World");
 console.log("Alfonso");
 
 //declaring variable x, setting it to 20
@@ -15,23 +15,73 @@ var my_age = 52;
 var userAge = prompt("How old are you?");
 console.log(userAge);
 
-if (userAge >= 21){
+*/
+
+/*var sleepy = false;
+var hungry = false;
+
+console.log(!sleepy);
+
+if(sleepy||hungry){
+  alert("Eat and get some rest");
+}*/
+
+function writeName(){
   var userName = prompt("What is your name?");
-  document.write("Welcome " + userName + "!");
+  //document.write();
+  document.getElementsById("welcome").innerHTML=("Welcome " + userName + "!");
 }
-else if(userAge >= 13){
-  var userName = prompt("What is your name?");
-  if(userName == "James"){
-    alert("Hello there James");
+
+function getUserAge(){
+  var userAge = prompt("What is your age?");
+  if (userAge >= 21){
+    writeName();
+  }
+  else if(userAge >= 13){
+    writeName();
+  }
+  else{
+    alert("You need parental permission");
   }
 }
+
+//A return statement returns a value and leaves the function immediately once it does
+
+function checkWord(guessedWord){
+  if(guessedWord == "sun"){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function guessingGame(){
+  var word = prompt("I'm thinking of a word with three letters that is in the sky");
+  var guessedCorrectly = checkWord(word);
+    if(guessedCorrectly){
+      alert("Correct word guessed");
+    }
+    else{
+      alert("Incorrect word guessed");
+    }
+}
+
+//guessingGame();
+//console.log(checkNumber());
+
+var human = "mortal";
+var god = "immortal";
+var socrates = god;
+
+if(socrates == human){
+  console.log("Socrates is mortal");
+}
 else{
-  alert("You need parental permission");
+  console.log("Socrates is not human");
 }
 
 
-
-
-
+//console.log(hungry);
 
 
